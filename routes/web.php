25466415d717b73c575485ed744fe35bc4d2113c
Javier Pedroza <javierlpedroza@qdata.io>
2017-05-08 18:12:43 -0500

    advance show vehiculos

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,11 @@ Route::get('/', array('as' => 'home', 'uses' => 'HomeController@welcome'));
 // Route::post('/entrada', 'HomeController@get_data_intro');
 Route::post('/entrada', array('as' => 'entrada', 'uses' => 'HomeController@get_data_intro'));
 
-
 Route::resource('estacionamientos', 'EstacionamientosController');
 
 Route::get('estacionamientos/{id}/delete', [
     'as' => 'estacionamientos.delete',
     'uses' => 'EstacionamientosController@destroy',
 ]);
+
+Route::get('/vehiculo/{id}', 'VehiculosController@show');
